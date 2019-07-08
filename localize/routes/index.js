a49527a91fa8381e887e216b85a776fbdc7501b4
Daniel Orio <danielislas3@live.com>
2019-07-08 17:25:46 -0500

    login done

--- localize/routes/index.js
+++ localize/routes/index.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const router  = express.Router();
+const router = require('express').Router()
+const { getSignup, postSignup, getLogin, postLogin, getProfile, logout } = require('../controllers/auth.controller')
+const { catchErrors } = require('../middlewares/handlers')
+const { isLoggedIn } = require('../middlewares/auth')
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
+router.get('/', (req, res, next) => res.render('index'))
 
-module.exports = router;
+router.get('/signup', getSignup)
+router.post('/signup', catchErrors(postSignup))
+
+router.get('/login', getLogin)
+router.post('/login', postLogin)
+
+router.get('/profile', isLoggedIn, getProfile)
+
+router.get('/logout', logout)
+
+module.exports = router
