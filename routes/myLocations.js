7ba94dd6f94d61dc118899b893aba1b1abbbf210
bichousPC <bichous@bichousPC.local>
2019-07-11 21:14:47 -0500

    agregando el crUd

--- routes/myLocations.js
+++ routes/myLocations.js
@@ -1,22 +1,32 @@
 const router = require('express').Router()
-const {isLoggedIn, checkLoggedUser}= require('../middlewares/auth')
-const {checkRole}= require('../middlewares/checkRole')
+const {
+    isLoggedIn,
+    checkLoggedUser
+} = require('../middlewares/auth')
+const {
+    checkRole
+} = require('../middlewares/checkRole')
 
 
 const {
     getLocations,
     getOneLocation,
     getCreateLocation,
-    postCreateLocation
+    postCreateLocation,
+    getEditLocation,
+    postEditLocation
 } = require('../controllers/owner.controller')
 
 //localhost:3000/myLocations
 /*************Crud *****************/
-router.get('/create',isLoggedIn, checkRole, getCreateLocation)
-router.post('/create',isLoggedIn, postCreateLocation)
+router.get('/create', isLoggedIn, checkRole, getCreateLocation)
+router.post('/create', isLoggedIn, postCreateLocation)
     /***********cRud ******************/
-router.get('/', isLoggedIn,checkRole,getLocations)
-router.get('/:id',isLoggedIn, getOneLocation)
+router.get('/', isLoggedIn, checkRole, getLocations)
+router.get('/:id', isLoggedIn, getOneLocation)
+    /****************crUd ****************/
+router.get('/:id', getEditLocation)
+router.post('/:id', postEditLocation)
 
 
 
